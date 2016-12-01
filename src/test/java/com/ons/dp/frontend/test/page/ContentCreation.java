6982ee5eaa34ae1d6bc2d140c0b1b82f3035c778
giri <giri.nattu@digital.hmrc.gov.uk>
2016-12-01 11:10:59 +0000

    Changes to selecting the content lists

--- src/test/java/com/ons/dp/frontend/test/page/ContentCreation.java
+++ src/test/java/com/ons/dp/frontend/test/page/ContentCreation.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by admin on 30/11/2016.
@@ -12,18 +11,27 @@ import java.util.List;
 public class ContentCreation extends BasePage {
 
     public String content_ids = "//li[@data-url='/economy')]]";
-    public By getContentIds(String content){
-      return  By.xpath(content_ids.replace("replace",content));
-    }
+    public String content_css = ".js-browse__item[data-url='/text_to_replace']>span>span";
     public By landingPage = By.cssSelector(".js-browse__item-title.page__item.page__item--taxonomy_landing_page");
     public By productpage = By.cssSelector(".js-browse__item-title.page__item.page__item--product_page");
     public By directory = By.cssSelector(".js-browse__item-title.page__item.page__item--directory");
     public ArrayList<WebElement> landingPageElements = new ArrayList<>();
     public ArrayList<WebElement> productPageElements = new ArrayList<>();
     public ArrayList<WebElement> directoryElements = new ArrayList<>();
+
+    public By getContentIds(String content) {
+        return By.xpath(content_ids.replace("replace", content));
+    }
+
+    public By getContentId(String text) {
+        String content = content_css.replace("text_to_replace", text);
+        return By.cssSelector(content);
+    }
+
     public void getLandingPageElements(){
         landingPageElements = (ArrayList<WebElement>) findElementsBy(landingPage);
     }
+
     public void getProductPageElements(){
         productPageElements = (ArrayList<WebElement>) findElementsBy(productpage);
     }
@@ -57,6 +65,10 @@ public class ContentCreation extends BasePage {
     }
 
     public void goToGDP(){
+        click(getContentId("economy"));
+        click(getContentId("economy/grossdomesticproductgdp"));
+
+
         getLandingPageElements();
         searchAndClickLP("Economy");
         getProductPageElements();
