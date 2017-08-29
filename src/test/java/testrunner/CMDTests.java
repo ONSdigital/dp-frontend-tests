72f07a3276a9d65f234a70977db8781254b90f4f
manojpathi <manojpathi@gmail.com>
2017-08-29 12:00:45 +0100

    Added CMD runner class and cmd profile im pom

---
+++ src/test/java/testrunner/CMDTests.java
@@ -0,0 +1,28 @@
+package testrunner;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@ExtendedCucumberOptions(
+        jsonReport = "target/webtest/cucumber.json",
+        detailedReport = true,
+        detailedAggregatedReport = true,
+
+        toPDF = true,
+        overviewReport = true,
+        outputFolder = "target/webtest")
+@CucumberOptions(
+        features = "src/test/resources/features/CMD",
+        plugin = {"html:target/webtest/cucumber-html-report/",
+                "json:target/webtest/cucumber.json", "pretty:target/webtest/cucumber-pretty.txt",
+                "usage:target/webtest/cucumber-usage.json", "junit:target/webtest/cucumber-results.xml"},
+        glue = "com.ons.dp.frontend.test",
+        // tags = {"@Media,@timeseriesgenerator"}
+        tags = {"@cpi"}
+)
+public class CMDTests extends BaseTestRunner{
+}
